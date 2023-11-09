8d026d8c924bb30021c9217041cbbcc398903c72
Patrick Dyer <pddyer21@gmail.com>
2023-11-08 23:11:33 -0500

    updated components

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const AllocationForm = (props) => {
-    const { dispatch,remaining  } = useContext(AppContext);
+    const { dispatch, remaining  } = useContext(AppContext);
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
@@ -42,7 +42,7 @@ const AllocationForm = (props) => {
                         <label className="input-group-text" htmlFor="inputGroupSelect01">Department</label>
                     </div>
                     <select className="custom-select" id="inputGroupSelect01" onChange={(event) => setName(event.target.value)}>
-                            <option defaultValue>Choose...</option>
+                            <option defaultValue >Choose...</option>
                             <option value="Marketing" name="marketing"> Marketing</option>
                             <option value="Sales" name="sales">Sales</option>
                             <option value="Finance" name="finance">Finance</option>
--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
 
 const Currency = () => {
+    const [Curr, setCurr] = useState('');
     
     return (
-        <div className='alert alert-success'>
-            <select className="custom-select" id="inputGroupCurr" >
-                            <option defaultValue>£ Pound</option>
-                            <option value="Marketing" name="marketing"> $ Dollar</option>
-                            <option value="Sales" name="sales">£ Pound</option>
-                            <option value="Finance" name="finance">&#8364; Euro</option>
-                            <option value="HR" name="hr">&#8377; Rupee</option>
-            </select>
-
-        </div>
+   
+            <div>  
+                <label className="alert alert-success" htmlFor="inputGroupCurr" style={{color: 'white', borderColor: '#d4edda', marginRight: '0px', borderRadius:"6px 0 0 6px", fontWeight:"bold"}} >Currency</label>
+                
+                <select className="alert alert-success" id="inputGroupCurr" onChange={(event) => setCurr(event.target.value)} style={{borderColor: '#d4edda', marginLeft: '0px', borderRadius: "0px 6px 6px 0px", fontWeight: "bold"}}>
+                                <option value="$ Dollar" name="Dollar" > $ Dollar</option>
+                                <option value="£ Pound" name="Pound" >£ Pound</option>
+                                <option value="&#8364; Euro" name="Euro" >&#8364; Euro</option>
+                                <option value="&#8377; Rupee" name="Rupee" >&#8377; Rupee</option>
+                </select>
+            </div> 
     )
             
 };
