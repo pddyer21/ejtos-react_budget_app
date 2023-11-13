e56ece99b2cf628ac7759c66280b3302ec9f2bd6
Patrick Dyer <pddyer21@gmail.com>
2023-11-13 10:20:00 -0500

    update Currency.js

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,21 +1,23 @@
-import React, { useContext, useState } from 'react';
-import { AppContext } from '../context/AppContext';
+import React, { useState } from 'react';
+
 
 
 const Currency = () => {
     const [Curr, setCurr] = useState('');
+
+
     
     return (
    
             <div>  
-                <label className="alert alert-success" htmlFor="inputGroupCurr" style={{color: 'white', borderColor: '#d4edda', marginRight: '0px', borderRadius:"6px 0 0 6px", fontWeight:"bold"}} >Currency</label>
-                
-                <select className="alert alert-success" id="inputGroupCurr" onChange={(event) => setCurr(event.target.value)} style={{borderColor: '#d4edda', marginLeft: '0px', borderRadius: "0px 6px 6px 0px", fontWeight: "bold"}}>
-                                <option value="$ Dollar" name="Dollar" > $ Dollar</option>
-                                <option value="£ Pound" name="Pound" >£ Pound</option>
-                                <option value="&#8364; Euro" name="Euro" >&#8364; Euro</option>
-                                <option value="&#8377; Rupee" name="Rupee" >&#8377; Rupee</option>
-                </select>
+                <label className="alert alert-success" htmlFor="inputGroupCurr" >Currency
+                    <select className="alert alert-success" id="inputGroupCurr" onChange={(event) => setCurr(event.target.value)}>
+                                    <option value="$" name="Dollar" > $ Dollar</option>
+                                    <option value="£" name="Pound" >£ Pound</option>
+                                    <option value="&#8364; Euro" name="Euro" >&#8364; Euro</option>
+                                    <option value="&#8377; Rupee" name="Rupee" >&#8377; Rupee</option>
+                    </select>
+                </label>
             </div> 
     )
             
